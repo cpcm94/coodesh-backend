e85fabbd2aab0ff47286d3f882d8e99291a16b3a
Caio Maia <caiopcmaia94@gmail.com>
2022-04-28 15:00:55 -0300

    search

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,10 +9,16 @@ class ArticlesController extends Controller
 {
     public function __invoke(Request $request) {
         $order = $request->order === 'asc' ? 'asc' : 'desc';
+        if (!!$request->search) {
+            $articles = Article::with(['launches', 'events'])->where('title', 'LIKE', '%'. $request->search . '%')->orderBy('id', $order)->paginate(10);
 
-        $articles = Article::with(['launches', 'events'])->orderBy('id', $order)->paginate(10);
 
-        return response($articles);
+            return response($articles);
+        } else {
+            $articles = Article::with(['launches', 'events'])->orderBy('id', $order)->paginate(10);
+            
+            return response($articles);
+        }
     }
 
 }
