ba40ff7f47b57bfe175109f89f5a8c69d68f7e6d
Caio Maia <caiopcmaia94@gmail.com>
2022-04-28 14:24:22 -0300

    orderBy

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    public function __invoke() {
+    public function __invoke(Request $request) {
+        $order = $request->order === 'asc' ? 'asc' : 'desc';
 
-        $articles = Article::with(['launches', 'events'])->paginate(10);
+        $articles = Article::with(['launches', 'events'])->orderBy('id', $order)->paginate(10);
 
         return response($articles);
     }
